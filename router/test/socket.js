6ffcea455b680db50c65e4d3248c9b7914f8611f
taehyundev <K.anon.11107@gmail.com>
2020-05-27 22:44:10 +0900

    update 2020-05-27

--- router/test/socket.js
+++ router/test/socket.js
@@ -4,5 +4,8 @@ var router = express.Router()
 router.get('/', (req,res)=>{
     res.render('test_socket.ejs')
 })
+io.on('connection', (socket) => {
+    console.log('a user connected');
+  });
 
 module.exports = router
